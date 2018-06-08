38cb98866806610b6c55ee40442447eca42fca87
Natalia0727 <oicafn@gmail.com>
2018-06-08 20:33:05 +0000

    automatically backed up by learn

---
+++ login.rb
@@ -0,0 +1,13 @@
+class User
+  
+  def name=(username)
+    @the_username = username
+  end
+  
+  def name
+    @the_username
+  end 
+end 
+jessica = User.new
+jessica.name="Jessica"
+puts jessica.name
